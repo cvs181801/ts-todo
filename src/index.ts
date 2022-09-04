6d87cb1f9e7c78f7d1ddd811f4f2eeee8a5b3399
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2022-09-04 16:05:37 -0700

    save progress on to do app

--- src/index.ts
+++ src/index.ts
@@ -8,20 +8,39 @@ confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
 })({ particleCount: 200, spread: 200 });
 
 const list = document.querySelector<HTMLUListElement>('#list');
-const form = document.getElementById('#new-task-form') as HTMLFormElement;
+const form = document.querySelector<HTMLFormElement>('#new-task-form');
 const titleInput = document.querySelector<HTMLInputElement>('#new-task-title');
 
 //create a way for the add button to add text to the array of to do items
 
 let toDoArray: string[] = [];
+let checkbox = document.createElement('input')
 
 form?.addEventListener("submit", e => {
   e.preventDefault();
-  console.log(e)
   if (titleInput?.value == null) return;
 
-  //toDoArray.push(titleInput.value);
-  console.log(titleInput.value)
+  // let task = {
+  //   taskTitle: titleInput.value,
+  //   checkbox: 
+
+
+  // }
+
+  //toDoArray.push(task);
+  
+  //console.log(titleInput.value)
+  console.log(toDoArray)
 })
 
+
+console.log('hey!')
+console.log(list)
+console.log(form)
+console.log(titleInput)
+
 //loop through array and render them all to the screen.  each should have 1. a checkbox 2. a delete button
+
+// toDoArray.forEach((todo)=>{
+//   let toDoItem: string = 
+// })
